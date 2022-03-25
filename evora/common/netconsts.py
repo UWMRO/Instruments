c83d4e44fe74e9a24f04198906d4b5985d46f182
Oliver Fraser <ojf@uw.edu>
2022-03-25 14:45:36 -0700

    Delete evora/common directory

--- evora/common/netconsts.py
+++
@@ -1,9 +0,0 @@
-# Global constants
-HEIMDALL_IP = "heimdall.mro.cwu.edu"
-GTCC_IP = "gtcc.mro.cwu.edu"
-FILTER_PI_IP = "telescopepi.mro.cwu.edu"
-
-CAMERA_PORT = 5502
-FTP_TRANSFER_PORT = 5504
-FTP_GET_PORT = 5505
-FILTER_PORT = 5503

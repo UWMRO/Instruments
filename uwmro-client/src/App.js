3365f5d2f9fc5e573dcc8bb912ec8322219a00fc
Eli L <sejglttle@gmail.com>
2022-08-11 10:29:56 -0700

    added ExposureTypeSelector

--- uwmro-client/src/App.js
+++ uwmro-client/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import ImageTypeSelector from './components/ImageTypeSelector';
 import GetTemp from './components/GetTemp';
+import ExposureTypeSelector from './components/SetExposureType';
 
 function App() {
   return (
     <div className='App'>
       <ImageTypeSelector/>
+      <ExposureTypeSelector/>
       <GetTemp/>
     </div>
   );

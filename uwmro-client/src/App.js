e74060be0d85b56c5c180e6616ae6e960fa03a8d
Eli L <sejglttle@gmail.com>
2022-08-14 13:58:10 -0700

    Added Filter Controls

--- uwmro-client/src/App.js
+++ uwmro-client/src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import ImageTypeSelector from './components/ImageTypeSelector';
 import GetTemp from './components/GetTemp';
 import ExposureTypeSelector from './components/SetExposureType';
+import FilterTypeSelector from './components/FilterControls';
 
 function App() {
   return (
     <div className='App'> Manastash Ridge Observatory Controls
       <ImageTypeSelector/>
       <ExposureTypeSelector/>
+      <FilterTypeSelector/>
       <GetTemp/>
     </div>
   );
